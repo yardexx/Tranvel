b9f06e9b87e897a1dd254ca099abc8c80aa08525
yardexx <62177414+yardexx@users.noreply.github.com>
2022-05-01 21:52:40 +0200

    [bloc] Adding bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:trainvel/home/home.dart';
-import 'package:trainvel/result/result.dart';
+import 'package:trainvel/result/cubit/trip_catalog_cubit.dart';
 
 void main() {
+  BlocOverrides.runZoned(
+    () => runApp(const App()),
+  );
   runApp(const App());
 }
 
@@ -13,26 +17,24 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => Catalog()),
-      ],
-      child: MaterialApp(
-        title: 'Trainvel',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorSchemeSeed: Colors.blue,
-          textTheme: GoogleFonts.montserratTextTheme(),
-          scaffoldBackgroundColor: Colors.grey.shade200,
-          cardTheme: CardTheme(
-            elevation: 8.0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
+    initializeDateFormatting();
+    return BlocProvider(
+        create: (_) => TripCatalogCubit(),
+        child: MaterialApp(
+          title: 'Trainvel',
+          theme: ThemeData(
+            useMaterial3: true,
+            colorSchemeSeed: Colors.blue,
+            textTheme: GoogleFonts.montserratTextTheme(),
+            scaffoldBackgroundColor: Colors.grey.shade200,
+            cardTheme: CardTheme(
+              elevation: 8.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
             ),
           ),
-        ),
-        home: const HomePage(),
-      ),
-    );
+          home: const HomePage(),
+        ));
   }
 }
